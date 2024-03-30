9f23b10c0c8855732b8eb25034897e5c9191fd9e
“b-zhao” <26752426+b-zhao@users.noreply.github.com>
2024-03-30 16:19:13 -0700

    add curvature's influence on the displacement of minima under distribution shift

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 Bo Zhao, Robert M. Gower, Robin Walters, Rose Yu. [Improving Convergence and Generalization Using Parameter Symmetries](https://arxiv.org/abs/2305.13404). *International Conference on Learning Representations (ICLR)*, 2024.
 
 ## Abstract
-In overparametrized models, different values of the parameters may result in the same loss value. Parameter space symmetries are transformations that change the model parameters but leave the loss invariant. Teleportation applies such transformations to accelerate optimization. However, the exact mechanism behind this algorithm’s success is not well understood. In this paper, we show that teleportation not only speeds up optimization in the short-term, but gives overall faster time to convergence. Additionally, we show that teleporting to minima with different curvatures improves generalization and provide insights on the connection between the curvature of the minima and generalization ability. Finally, we show that integrating teleportation into a wide range of optimization algorithms and optimization-based meta-learning improves convergence.
+In many neural networks, different values of the parameters may result in the same loss value. Parameter space symmetries are loss-invariant transformations that change the model parameters. Teleportation applies such transformations to accelerate optimization. However, the exact mechanism behind this algorithm's success is not well understood. In this paper, we show that teleportation not only speeds up optimization in the short-term, but gives overall faster time to convergence. Additionally, teleporting to minima with different curvatures improves generalization, which suggests a connection between the curvature of the minimum and generalization ability. Finally, we show that integrating teleportation into a wide range of optimization algorithms and optimization-based meta-learning improves convergence. Our results showcase the versatility of teleportation and demonstrate the potential of incorporating symmetry in optimization.
 
 ## Requirements 
 * [PyTorch](https://pytorch.org/)
 * [Matplotlib](https://matplotlib.org/)
 * [SciPy](https://scipy.org/install/)
+* [Shapely](https://shapely.readthedocs.io/en/stable/)
 
 ## Initializing directories
 ```
@@ -15,10 +16,16 @@ python init_directory.py
 ```
 
 ## Reproducing experiments in the paper
-Correlation between sharpness/curvature and validation loss (Table 1, Figure 10, Figure 11):
+Correlation between sharpness/curvature and validation loss (Table 1, Figure 9, Figure 10):
 
 ```
 python correlation.py
+```
+
+How curvature influences the expected displacement of minima under distribution shifts (Figure 7):
+
+```
+python displacement_integration.py
 ```
 
 Teleportation to change sharpness or curvature (Figure 4):
@@ -27,7 +34,7 @@ Teleportation to change sharpness or curvature (Figure 4):
 python teleport_sharpness_curvature.py
 ```
 
-Integrating teleportation with various optimizers (Figure 5, Figure 14):
+Integrating teleportation with various optimizers (Figure 5, Figure 13):
 
 ```
 python teleport_optimization.py
---
+++ displacement_integration.py
@@ -0,0 +1,79 @@
+# Script for computing the expected distance between w0 and a curve as a function of the curvature at w0.
+
+import matplotlib.pyplot as plt
+import numpy as np
+import shapely.geometry as geom
+from scipy.integrate import quad, nquad
+
+# define integrand = dist((r\cos{\theta}, r\sin{\theta}), \gamma) r
+def integrand(t, r, gamma):
+    point = geom.Point(r * np.cos(t), r * np.sin(t))
+    dist = point.distance(gamma) * r
+    return dist
+
+# set up color maps
+N = 5
+base_cmaps = ['Blues','Oranges']
+n_base = len(base_cmaps)
+colors = np.concatenate([plt.get_cmap(name)(np.linspace(0.2,0.8,N)) for name in base_cmaps])
+
+
+# gamma: (x, kx^2)
+plt.figure()
+plt.rcParams["axes.prop_cycle"] = plt.cycler("color", colors)
+for r in [2e0, 1e0, 5e-1, 2e-1, 1e-1]:
+    k = np.arange(0, 40, 0.3)
+    int_dist = np.zeros_like(k)
+    for i in range(len(int_dist)):
+        # construct gamma
+        x_arr = np.arange(-10, 10, 0.1)
+        y_arr = k[i] * np.abs(x_arr**2)
+
+        point_arr = []
+        for p_idx in range(len(x_arr)):
+            point_arr.append(geom.Point(x_arr[p_idx], y_arr[p_idx]))
+        parabola = geom.LineString(point_arr)
+
+        # compute \int_0^{2\pi} dist((r\cos{\theta}, r\sin{\theta}), \gamma) r d\theta
+        int_dist[i] = quad(integrand, 0, 2*np.pi, args=(r, parabola))[0]
+
+    # plot int_dist / (2 \pi r) / r. The additional r aligns all curves in one plot.
+    plt.plot(2*k, int_dist / (2 * np.pi * r * r), linewidth=3, label='r={}'.format(r))
+
+plt.xlabel(chr(954)+r'$ = 2k_1$', fontsize=26)
+plt.ylabel(r'$\frac{1}{r}\mathbb{E}_{S_r} dist(w, \gamma_1)$', fontsize=26)
+plt.xticks([0, 20, 40, 60, 80], fontsize=20)
+plt.yticks([0.625, 0.675, 0.725, 0.775], fontsize=20)
+plt.legend(fontsize=20)
+plt.savefig('figures/curvature_displacement_integral_kx_sqr.pdf', dpi=400, bbox_inches='tight')
+
+
+# gamma: x^2 + y^2 = k^2
+plt.figure()
+plt.rcParams["axes.prop_cycle"] = plt.cycler("color", colors)
+for r in [1e-1, 5e-2, 2e-2, 1e-2, 5e-3]:
+    k = np.concatenate((np.arange(0.1, 1.0, 0.01), np.arange(1.0, 10.0, 0.1)))
+    int_dist = np.zeros_like(k)
+    for i in range(len(int_dist)):
+        # construct gamma
+        theta_arr = np.arange(0, 2*np.pi, 1e-3)
+        x_arr = k[i] * np.cos(theta_arr)
+        y_arr = k[i] * np.sin(theta_arr) + k[i]
+
+        point_arr = []
+        for p_idx in range(len(x_arr)):
+            point_arr.append(geom.Point(x_arr[p_idx], y_arr[p_idx]))
+        gamma = geom.LineString(point_arr)
+
+        # compute \int_0^{2\pi} dist((r\cos{\theta}, r\sin{\theta}), \gamma) r d\theta
+        int_dist[i] = quad(integrand, 0, 2*np.pi, args=(r, gamma), limit=500)[0]
+        
+    # plot int_dist / (2 \pi r) / r. The additional r aligns all curves in one plot.
+    plt.plot(1 / k, int_dist / (2 * np.pi * r * r), linewidth=3, label='r={}'.format(r))
+
+plt.xlabel(r'$\kappa = \frac{1}{k_2}$', fontsize=26)
+plt.ylabel(r'$\frac{1}{r}\mathbb{E}_{S_r} dist(w, \gamma_2)$', fontsize=26)
+plt.xticks(fontsize= 20)
+plt.yticks([0.6, 0.61, 0.62, 0.63], fontsize=20)
+plt.legend(fontsize=20)
+plt.savefig('figures/curvature_displacement_integral_circle.pdf', dpi=400, bbox_inches='tight')
